8537a6873611085d54deddddd64bbd9b8789c1e2
Evgeny Bukreev <ebukreev@ptsecurity.com>
2026-01-23 17:53:43 +0300

    refactor TMs

--- src/TM.v
+++ src/TM.v
@@ -1,50 +1,101 @@
-From Coq Require Fin.
+Set Warnings "-stdlib-vector".
+From Coq Require Vectors.Vector.
 From klenee_complexity Require Import NEList.
 
-Class TMParams := {
-  Q : Type;
-  Σ : Type;
-  Γ : Type;
-  work_tapes_num : nat;
-}.
-
 Section TuringMachines.
-    Context `{TMParams}.
-
-    Inductive Move := Left | Right | Stay. 
-
-    Record TM (TransitionsFunc : nat -> Type) := {
-        init : Q;
-        final : Q -> bool;
-        transitions : TransitionsFunc work_tapes_num;
+    Class TMParams := {
+        Q : Type;
+        Σ : Type;
+        Γ : Type;
+        work_tapes_num : nat;
     }.
 
-    Definition DTransitions (n : nat) :=
-        Q * Σ * (Fin.t n -> Γ) -> 
-        Q * (Fin.t n -> Γ) * Move * (Fin.t n -> Move).
-
-    Definition NTransitions (n : nat) :=
-        Q * Σ * (Fin.t n -> Γ) -> 
-        list (Q * (Fin.t n -> Γ) * Move * (Fin.t n -> Move)).
-
-    Definition DTM := TM DTransitions.
-    Definition NTM := TM NTransitions.
-
-    Inductive QType := Existential | Universal.
-
-    Record ATM := {
-        ntm : NTM;
-        q_type : Q -> QType;
-    }.
+    Context `{params : TMParams}.
 
     Record Conf := {
         state : Q;
         input : list Σ;
         input_index : nat;
-        work_tapes : Fin.t work_tapes_num -> list Γ;
-        work_tapes_indices : Fin.t work_tapes_num -> nat;
+        work_tapes : Vector.t (list Γ) work_tapes_num;
+        work_tapes_indices : Vector.t nat work_tapes_num;
     }.
 
-    Definition Trace := NEList Conf.
+    Definition Trace  := NEList Conf.
 
+    Class TM := {
+        init : Q;
+        final : Q -> bool;
+
+        transitions : Type;
+        do_transition : Conf -> Conf;
+    }.
+
+    Record DTM := {
+        d_init : Q;
+        d_final : Q -> bool;
+    }.
+
+    Inductive Move := Left | Right | Stay. 
+
+    Definition DTransitions (n : nat) :=
+        Q * Σ * (Vector.t Γ n) -> 
+        Q * (Vector.t Γ n) * Move * (Vector.t Move n).
+
+    Global Instance DTM_TM (dtm : DTM) : TM :=
+    {|
+        init := d_init dtm;
+        final := d_final dtm;
+
+        transitions := DTransitions work_tapes_num;
+        do_transition := id; (*TODO*)
+    |}.
+
+    Record NTM := {
+        n_init : Q;
+        n_final : Q -> bool;
+    }.
+
+    Definition NTransitions (n : nat) :=
+        Q * Σ * (Vector.t Γ n) -> 
+        list (Q * (Vector.t Γ n) * Move * (Vector.t Move n)).
+
+    Global Instance NTM_TM (ntm : NTM) : TM :=
+    {|
+        init := n_init ntm;
+        final := n_final ntm;
+
+        transitions := NTransitions work_tapes_num;
+        do_transition := id; (*TODO*)
+    |}.
+
+    Inductive QType := Existential | Universal.
+
+    Record ATM := {
+        a_init : Q;
+        a_final : Q -> bool;
+
+        q_type : Q -> QType;
+    }.
+
+    Global Instance ATM_TM (atm : ATM) : TM :=
+    {|
+        init := a_init atm;
+        final := a_final atm;
+
+        transitions := NTransitions work_tapes_num;
+        do_transition := id; (*TODO*)
+    |}.
 End TuringMachines.
+
+Definition transition `{TMParams} (tm : TM) (conf1 conf2 : Conf) : Prop :=
+    do_transition conf1 = conf2.
+
+Notation "conf1 ⊢ᶜ conf2" := (transition _ conf1 conf2) (at level 70).
+
+Definition ttransition `{TMParams} (tm : TM) (trace1 trace2 : Trace) : Prop :=
+    let head1 := head trace1 in
+    let head2 := head trace2 in
+        trace2 = cons head2 (trace1) /\
+        head1 ⊢ᶜ head2.
+
+Notation "trace1 ⊢ trace2" := (ttransition _ trace1 trace2) (at level 70).
